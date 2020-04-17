831a910076c4a783c38cbe39c31a6bc73b5fb4b5
GideonMoses11 <gideonmoses11@gmail.com>
2020-04-17 21:24:27 +0100

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+<div>
+<b-nav tabs>
+    <b-nav-item disabled><b><h5>Gedoni's Fancy Quiz App</h5></b></b-nav-item>
+    <b-nav-item disabled>Counter: {{ numCorrect }}/{{ numTotal }}</b-nav-item>
+  </b-nav>
+</div>
+</template>
+
+
+<script>
+export default {
+  props: [
+    'numCorrect',
+    'numTotal'
+  ]
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,23 @@
 import Vue from 'vue'
+import BootstrapVue from 'bootstrap-vue'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
 import App from './App.vue'
 
+import VueLodash from 'vue-lodash'
+
+// import Lodash from 'lodash'
+
+
+
+window._ = require('lodash');
+
+Vue.use(VueLodash)
+Vue.use(BootstrapVue)
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+    render: h => h(App),
 }).$mount('#app')
